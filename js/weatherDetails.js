f7765c5293c58e2e704c9b42e786fc4d6ddc0113
Fabian Nunes <fabiannunes12@gmail.com>
2022-09-28 09:45:24 +0100

    Deatils page card

--- js/weatherDetails.js
+++ js/weatherDetails.js
@@ -5,12 +5,52 @@ $(document).ready(function() {
     const urlParams = new URLSearchParams(query);
     const city = urlParams.get('city');
 
-    //let weather = getWeather(city);
+    let weather = getWeather(city);
+    weather.then(data => {
+        const city = document.getElementById('city');
+        city.innerHTML = data.name;
 
+        //const country = document.getElementById('country');
+        //country.innerHTML = data.sys.country;
+
+        const hourDate = document.getElementById('hourDate');
+        hourDate.innerHTML = new Date(data.dt * 1000).toLocaleString();
+
+        const temp = document.getElementById('temp');
+        temp.innerHTML = data.main.temp + 'ºC';
+
+        const humidity = document.getElementById('humidity');
+        humidity.innerHTML = data.main.humidity + '%';
+
+        const wind = document.getElementById('wind');
+        wind.innerHTML = data.wind.speed + 'm/s';
+
+        const clouds = document.getElementById('clouds');
+        clouds.innerHTML = data.clouds.all + '%';
+
+        const pressure = document.getElementById('pressure');
+        pressure.innerHTML = data.main.pressure + 'hPa';
+
+        const coord = document.getElementById('coord');
+        coord.innerHTML = data.coord.lat + 'º, ' + data.coord.lon + 'º';
+
+        const weatherIcon = document.getElementById('weatherIcon');
+        weatherIcon.title = data.weather[0].description;
+
+
+        weatherIcon.src = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
+    });
 });
 
-async function getWeather(id) {
+async function getWeatherForecast(id) {
     const base = 'https://api.openweathermap.org/data/2.5/forecast';
+    const query = `?id=${id}&units=metric&appid=${key}`;
+    const response = await fetch(base + query);
+    return await response.json();
+}
+
+async function getWeather(id) {
+    const base = 'https://api.openweathermap.org/data/2.5/weather';
     const query = `?id=${id}&units=metric&appid=${key}`;
     const response = await fetch(base + query);
     return await response.json();
